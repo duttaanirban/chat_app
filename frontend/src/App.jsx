a49f0a8b05d287813a33393eec4b2ebdf13fdb81
Anirban Dutta <duttaanirbanad@gmail.com>
2025-02-15 00:22:32 +0530

    chatapp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from './store/useAuthStore';
+const App = () => {
+  const {authUser} = useAuthStore()
+  return (
+    <div>
+    
+
+    <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/settings' element={<SettingsPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+    </Routes>
+
+
+
+
+    </div>
+  )
+}
+
+export default App
