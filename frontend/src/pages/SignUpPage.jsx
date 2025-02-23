558977cadcb319dbfcd377e62baba64d09339dd8
duttaanirban <duttaanirbanad@gmail.com>
2025-02-23 16:58:25 +0530

    new

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -103,6 +103,17 @@ const SignUpPage = () => {
                 </button>
               </div>
             </div>
+
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
             </form>
         </div>
 
